6e370ad6419fdbea7213103139737784d77441f3
Eszes Bálint <eszes.balint@pm.me>
2023-07-24 15:45:12 +0200

    Delete js directory

--- js/commands/commands.js
+++
@@ -1,8 +0,0 @@
-import { AddCommand } from './add.js';
-import { DeleteCommand } from './delete.js';
-import { GroupCommand, UngroupCommand } from './group.js';
-import { TransformCommand } from './transform.js';
-import { DuplicateCommand } from './duplicate.js';
-import { PropertyCommand } from './property.js';
-
-export { AddCommand, DeleteCommand, GroupCommand, UngroupCommand, TransformCommand, DuplicateCommand, PropertyCommand };
--- js/elements/elements.js
+++
@@ -1,6 +0,0 @@
-import { BlockDisplay } from "./BlockDisplay";
-import { ItemDisplay } from "./ItemDisplay";
-import { TextDisplay } from "./TextDisplay";
-import { Collection } from "./collection";
-
-export { BlockDisplay, ItemDisplay, TextDisplay, Collection };
